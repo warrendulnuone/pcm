7a357096cb7a36d583c8372dc0c3b7012b999598
DOORSCOMPUTERS\Administrator <warrend@doorscomputers.com>
2019-10-04 06:08:46 +0800

    Initial Commit Oct 4 2019

---
+++ ckeditor/plugins/doksoft_image_embed/lang/en.js
@@ -0,0 +1,15 @@
+CKEDITOR.plugins.setLang(
+	
+	'doksoft_image_embed',
+	
+	'en',
+	{	
+		doksoft_image_embed:
+		{
+			button_label: "Insert embedded image",
+			dlg_title: "Insert embedded image",
+			not_supported: 'The File APIs are not fully supported in this browser',
+			choose_files: 'Choose files first!'
+		}
+	}
+);
